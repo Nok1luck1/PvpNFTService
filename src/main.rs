e3f65f7e37aba20320b095530522c200534d41d3
Nok1luck1 <kolambo4ka@gmail.com>
2023-05-14 15:06:38 +0300

    sdvsddv

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,16 @@ use web3::types::{Address, FilterBuilder, H160, U256};
 #[tokio::main]
 async fn main() -> web3::Result<()> {
     dotenv::dotenv().ok();
-
+    let websokcet = web3::transports::WebSocket::new(&env::var("INFURA_SEPOLIA").unwrap()).await?;
+    let web3s = web3::Web3::new(websokcet);
+    let mut accounts = web3s.eth().accounts().await?;
+    println!("{:?}", accounts);
+    let contract = Contract::from_json(
+        web3s,
+        Address::from("value"),
+        ("../artifacts/contracts/FishNFT.json"),
+    )
+    .unwrap()
+    .await?;
     Ok(())
 }
